d8ffac2ae60d30aef6fab0734b84adfe425e70a6
Lokki-ki-ki <74096861+Lokki-ki-ki@users.noreply.github.com>
2024-03-24 13:41:41 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from .app import create_app
 from asgiref.wsgi import WsgiToAsgi
 
-# app = create_app()
+app = create_app()
+app.run(debug=True, host='0.0.0.0', port='8080', threaded=True)
 # asgi_app = WsgiToAsgi(app)
 
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port='8080', threaded=True)
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(debug=True, host='0.0.0.0', port='8080', threaded=True)
