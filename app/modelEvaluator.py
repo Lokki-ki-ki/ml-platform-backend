42fce506558dc7fc09189b7df773911cef9ad00f
Lokki-ki-ki <74096861+Lokki-ki-ki@users.noreply.github.com>
2024-03-28 09:59:26 +0800

    Align api request with contract & oracle

--- app/modelEvaluator.py
+++ app/modelEvaluator.py
@@ -5,34 +5,54 @@ import numpy as np
 import h5py
 import asyncio
 import aiohttp
+from itertools import combinations
 from app.utils import download_file_from_ipfs, download_model_from_ipfs, initialize_sample_model
 from app.utils import download_weights_from_ipfs_async
 
 ########## Configurations ##########
 TEST_DATA = "QmZxr5vzCirovKm2sGXqo1rHvC9mfsNeTMXjos9fcQri6X"
 TEST_LABELS = "QmaqsdE1hrRWMRsV4eXpYfe4CnmALDLTaL97mVCnaZu1ST"
-# MODEL = 
 ####################################
 
+# Example request body:
+# {
+#     "clientsToSubmissions":{"1":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj","2":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj","3":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj"},
+#     "clientsToReputation":{"1":"100","2":"100","3":"100"},
+#     "rewardPool":"999999999999999900",
+#     "modelAddress":"0x1234567890123456789012345678901234567890",
+#     "testDataAddress":"0x1234567890123456789012345678901234567890",
+#     "testLabelAddress":"0x1234567890123456789012345678901234567890",
+#     "testDataHash":"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+#     "testLabelHash":"0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890"
+# }
+
 class MlModel:
-    def __init__(self, client_weights, test_data_add=TEST_DATA, test_labels_add=TEST_LABELS, model_address=None):
+    def __init__(self, clients_submissions, test_data_add=TEST_DATA, test_labels_add=TEST_LABELS, model_address=None, reward_pool=None, clients_reputation=None, test_data_hash=None, test_label_hash=None):
         # convert matrixSize and weights to numpy array
         if model_address:
             self.model = download_model_from_ipfs(model_address)
         else: # TODO: remove this else block
             self.model = initialize_sample_model()
         logging.info("Initialize the model successfully.")
-        self.clientToWeights = asyncio.run(self.download_clients_weights(client_weights))
+        self.clientToWeights = asyncio.run(self.download_clients_weights(clients_submissions))
         self.download_test_data(test_data_add, test_labels_add)
         logging.info("Download the weights & data successfully.")
         self.evaluation_results = asyncio.run(self.main_evaluation())
         logging.info("Evaluation results are ready.")
 
     def get_evaluation_results(self):
-        cilentToResults = {}
-        for tuple in self.evaluation_results:
-            cilentToResults[tuple[0]] = tuple[1]
-        return cilentToResults
+        dummy_results = {
+            "newModelAddress": "QmYEMkTVdYF7bBoJ28D2Lrqex1xozLZ5yHQ8pjDuJ18zQe",
+            "clientIds": ["1", "2", "3", "4", "5"],
+            "clientNewReputations": [100, 100, 100, 90, 80],
+            "clientRewards": [100, 100, 100, 90, 80]
+        }
+        # results = {}
+        # results["newModelAddress"] = self.newModelAddress
+        # results["clientIds"] = self.evaluate_results.keys()
+        # results["clientNewReputations"] = TODO
+        # results["clientRewards"] = TODO
+        return dummy_results
             
     def generate_weights_template(self, sample_weights):
         layers = []
@@ -41,11 +61,15 @@ class MlModel:
         return layers
     
     async def download_clients_weights(self, client_weights):
+        """
+        Convert client_id: weights_ipfs_address to client_id: weights_local_path
+        """
+        # client_weights : client_id: weights_address
         clientsToWeights = {}
         tasks = []
         async with aiohttp.ClientSession() as session:
-            for i in range(len(client_weights)):
-                task = asyncio.create_task(download_weights_from_ipfs_async(session, client_weights[i], i))
+            for client_id, weights_address in client_weights.items():
+                task = asyncio.create_task(download_weights_from_ipfs_async(session, weights_address, client_id))
                 tasks.append(task)
             paths = await asyncio.gather(*tasks)
         # return paths to client - path map
@@ -81,11 +105,57 @@ class MlModel:
         Evaluate the model with the given weights
         """
         tasks = []
+        all_weights_paths = self.clientToWeights.values()
         for client_id, weights_path in self.clientToWeights.items():
             task = asyncio.create_task(self.evaluate_model(weights_path, client_id))
             tasks.append(task)
         results = await asyncio.gather(*tasks)
         return results
+    
+    def get_all_combinations(self, clients_id):
+        """
+        Returns [["1", "2", "3], ["2", "3"]]...
+        """
+        all_combinations = []
+        for i in range(1, len(clients_id)+1):
+            all_combinations.extend(list(combinations(clients_id, i)))
+        return all_combinations
+    
+    def sort_tuple(self, tup):
+        """
+        Input: a tuple
+        Output: a sorted string
+        """
+        result = list(tup)
+        result.sort()
+        str_result = ''.join(list(map(lambda x: str(x), result)))
+        return str_result
+    
+    def calculate_avg_weights(self, dict_of_weights, tup):
+        weights = [dict_of_weights[i] for i in tup]
+        avg_weights = []
+        weight_length = len(weights[0])
+        for i in range(weight_length):
+            single_sum = 0
+            for weight in weights:
+                single_sum += weight[i]
+            avg_weights.append(single_sum / len(weights))
+            # avg_weights.append(sum([weight[i] for weight in weights]) / len(weights))
+        return avg_weights
+    
+    async def calculate_accuracy_for_permutations(self, permutations, dict_of_weights, initial_weights):
+        """
+        Input: a dictionary of permutations and a dictionary of weights
+        Output: a dictionary of permutations and their corresponding accuracy
+        """
+        dict_of_acc = {}
+        for tup in permutations:
+            key = self.sort_tuple(tup)
+            avg_weights = self.calculate_avg_weights(dict_of_weights, tup)
+            acc = await self.evaluate_model
+            dict_of_acc[key] = acc[1]
+        return dict_of_acc
+
     
     def aggregate_new_models(self):
         """
--- app/routes/evaluator.py
+++ app/routes/evaluator.py
@@ -7,23 +7,48 @@ class Evaluator(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         # validate the input, if not valid, return 401
-        parser.add_argument("sampleModelAddress", type=str)
+        # Example request body:
+        # {
+        #     "clientsToSubmissions":{"1":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj","2":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj","3":"QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj"},
+        #     "clientsToReputation":{"1":"100","2":"100","3":"100"},
+        #     "rewardPool":"999999999999999900",
+        #     "modelAddress":"0x1234567890123456789012345678901234567890",
+        #     "testDataAddress":"0x1234567890123456789012345678901234567890",
+        #     "testLabelAddress":"0x1234567890123456789012345678901234567890",
+        #     "testDataHash":"0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+        #     "testLabelHash":"0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890"
+        # }
+        parser.add_argument("clientsToSubmissions", type=dict)
+        parser.add_argument("clientsToReputation", type=dict)
+        parser.add_argument("rewardPool", type=str)
+        parser.add_argument("modelAddress", type=str)
         parser.add_argument("testDataAddress", type=str)
         parser.add_argument("testLabelAddress", type=str)
-        parser.add_argument("clientWeights", action='append')
+        parser.add_argument("testDataHash", type=str)
+        parser.add_argument("testLabelHash", type=str)
         args = parser.parse_args()
-        if not args['clientWeights']:
-            return {'error': 'clientWeights is required'}, 401
+        if not args['clientsToSubmissions']:
+            return {'error': 'clientsToSubmissions is required'}, 401
+        if not args['clientsToReputation']:
+            return {'error': 'clientsToReputation is required'}, 401
+        if not args['rewardPool']:
+            return {'error': 'rewardPool is required'}, 401
+        if not args['modelAddress']:
+            return {'error': 'modelAddress is required'}, 401
         if not args['testDataAddress']:
             return {'error': 'testDataAddress is required'}, 401
         if not args['testLabelAddress']:
             return {'error': 'testLabelAddress is required'}, 401
-        # if not args['sampleModelAddress']:
-        #     return {'error': 'sampleModelAddress is required'}, 401
-        model = MlModel(args['clientWeights'], args['testDataAddress'], args['testLabelAddress']) #TODO: Add model address
+        
+        # def __init__(self, client_weights, test_data_add=TEST_DATA, test_labels_add=TEST_LABELS, model_address=None, reward_pool=None, clientsToReputation=None, clientsToSubmissions=None, test_data_hash=None, test_label_hash=None):
+        model = MlModel(args['clientsToSubmissions'], args['testDataAddress'], args['testLabelAddress'], args['modelAddress'], args['rewardPool'], args['clientsToReputation'], args['testDataHash'], args['testLabelHash']) #TODO: Add model address
         results = model.get_evaluation_results()
-        new_model = "QmXvmaD8FuPnySgNaxv3vun9ZtuMGdDFnNS6tsLKz8Jhyj"
-        return {'results': results, 'new_model': new_model}, 200
+        # Results shoule contains:
+        # const newModelAddress = "0x1234567890123456789012345678901234567890";
+        # const clientIds = [1, 2, 3];
+        # const clientNewReputations = [100, 100, 100];
+        # const clientRewards = [80, 10, 10];
+        return results, 200
     
     def get(self):
         return {'message': 'The model evaluator is running!'}, 200
